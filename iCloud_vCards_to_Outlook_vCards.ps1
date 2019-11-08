fbdb73aa86e25529754511bce55880fb8c7e1f41
schpanky <schpanky@gmail.com>
2019-11-08 07:47:12 -0500

    Create iCloud_vCards_to_Outlook_vCards.ps1

---
+++ iCloud_vCards_to_Outlook_vCards.ps1
@@ -0,0 +1,24 @@
+﻿clear-host
+$ifile = $args[0]
+
+if($ifile -eq $NULL)
+    {
+        Write-Host How to use: .\iCloud_vCards_to_Outlook_vCards.ps1 filenameofvcard.vcf
+        Exit
+    }
+
+    Write-Host Output: $ifile
+    $i = 1
+    switch -regex -file $ifile
+    {
+
+        "^BEGIN:VCARD" {
+            if($FString){
+                $FString | out-file -Encoding "ASCII" "$ifile.$i.vcf"};
+                $FString = $_;$i++
+            }
+        "^(?!BEGIN:VCARD)" {$FString += "`r`n$_"}
+    }
+
+Write-Host Fin.
+Write-Host $i VCards
